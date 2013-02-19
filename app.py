e68260811ba4e6e8951c4d4408f9a1be7dc3fa97
Alex Paz <alexander.j.paz@ngc.com>
2013-02-19 15:28:34 -0500

    Switched to flask, finally

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask, request
-app = Flask(__name__, static_url_path='')
+from flask import Flask, url_for,render_template, send_from_directory
+app = Flask(__name__)
 
 @app.route('/')
 def root():
-return tandem.send_static_file('index.html')
+	return send_from_directory('static', 'index.html')
+
+@app.route('/hangout')
+def hangout():
+    templ = render_template('hangout.xml')
+    return templ
+
+@app.route('/assets/<path:filename>')
+def assets(filename):
+    return	send_from_directory('static/assets', filename)
+
+@app.route('/hello')
+def hello():
+   return "Okay, you aren't crazy"
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
