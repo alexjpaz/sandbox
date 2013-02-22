937b6bb959ea39f8f868bdbd182b5d4a3e164fdf
Alex Paz <alexjpaz@gmail.com>
2013-02-21 21:39:59 -0500

    Styling and logic

--- static/assets/js/dicegame.js
+++ static/assets/js/dicegame.js
@@ -1,23 +1,20 @@
 (function(){
 	var game = angular.module('DiceGame',['Utilities']);
 	
-	game.factory('DiceGame', function(GameException) {
+	game.factory('DiceGame', function(DiceSet, GameException) {
 		function DiceGame() {
-			this.bag = ['%','$','^','A','V'];
+			this.bag = new DiceSet();
 			this.board = [];
-			
-			this.__random = {};
+
 		}
 		
 		DiceGame.prototype.rollDie = function() {
-			if(this.bag == 0) {
+			console.log(Math.random().toString(36).substring(7));
+			if(this.bag.size() == 0) {
 				throw new NoDiceLeftException();
 			}
-			
-			this.__random.index = Math.floor(Math.random()*this.bag.length);
-			this.__random.die = this.bag.splice(this.__random.index, 1)[0];
-			
-			this.board.push(this.__random.die);
+		
+			this.board.push(this.bag.pick());
 		}
 		
 		function NoDiceLeftException() {
@@ -26,6 +23,37 @@
 		}
 		
 		return DiceGame;
+	});
+	
+	game.factory('DiceSet', function() {
+		function DiceSet() {
+			this._set = this.generateSet(10);
+			this.__random = {};
+		}
+		
+		DiceSet.prototype.pick = function() {
+			this.__random.index = Math.floor(Math.random()*this._set.length);
+			this.__random.die = this._set.splice(this.__random.index, 1)[0];
+			
+			return this.__random.die;
+		}
+		
+		DiceSet.prototype.generateSet = function(amount) {
+			var text = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789`~!@#$%^&*()_+-=[]{}\'"\\;:<>,./?';
+			var set = [];
+			
+			for(var i=0;i<amount;i++) {
+				set.push(text.charAt(Math.floor(Math.random() * text.length)));
+			}
+			
+			return set;
+		}
+		
+		DiceSet.prototype.size = function() {
+			return this._set.length;
+		}
+		
+		return DiceSet;
 	});
 	
 	game.controller('DiceGameCtrl', function($scope, DiceGame){
